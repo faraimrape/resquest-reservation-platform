4bf8f47c37fc2ad2dda2f70fabfcf8936b500ff4
Farai Mrape <farai.mrape@liquorice.co.za>
2024-08-21 17:37:30 +0200

    Add authentication guards and other small fixes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
-        vue(), // Add the Vue plugin here
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
